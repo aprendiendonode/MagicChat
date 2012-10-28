95f5d839e9b2e522d44a94415a260d9fa32d702b
dannegm <danneg.m@gmail.com>
2012-10-27 21:37:46 -0500

    bug fixes

--- magicchat/js/script.js
+++ magicchat/js/script.js
@@ -1,5 +1,41 @@
 
-var n = prompt("Tu nombre", "");
+function hora () {
+	var horas = new Date(),
+		ampm = 'am',
+		hours = horas.getHours(),
+		minutes = horas.getMinutes();
+	if (hours > 12){
+		ampm = 'pm';
+	}
+	switch(hours){
+		case 13: hours = 1; break;
+		case 14: hours = 2; break;
+		case 15: hours = 3; break;
+		case 16: hours = 4; break;
+		case 17: hours = 5; break;
+		case 18: hours = 6; break;
+		case 19: hours = 7; break;
+		case 20: hours = 8; break;
+		case 21: hours = 9; break;
+		case 22: hours = 10; break;
+		case 23: hours = 11; break;
+		case 24: hours = 12; break;
+	}
+	switch(minutes){
+		case 1: minutes = '01'; break;
+		case 2: minutes = '02'; break;
+		case 3: minutes = '03'; break;
+		case 4: minutes = '04'; break;
+		case 5: minutes = '05'; break;
+		case 6: minutes = '06'; break;
+		case 7: minutes = '07'; break;
+		case 8: minutes = '08'; break;
+		case 9: minutes = '09'; break;
+	}
+	return hours + ':' + minutes + ' ' + ampm;
+}
+
+n = prompt("Tu nombre", "");
 
 var sonidito = document.createElement('audio');
 	sonidito.src = "http://soundjax.com/reddo/27947%5EBells.mp3";
@@ -120,7 +156,7 @@ if (n == null || n == ""){
 				.replace(/\]\]/g, '</code>');
 
 			if(msgg == 'si'){
-				$('#logs').append('<article class="msg"><strong>' + user.nombre + '</strong><span>' + user.texto + '</span></article>');
+				$('#logs').append('<article class="msg"><span class="time">' + hora() + '</span><strong>' + user.nombre + '</strong><span>' + user.texto + '</span></article>');
 			}
 
 			var altodiv = $('#logs').height();
@@ -189,6 +225,8 @@ if (n == null || n == ""){
 			}
 		}else{
 			$('#logs').append('<article class="blue"><strong>' + newname.last + '</strong> se ha cambiado el nombre a <strong>' + newname.now + '</strong></span></article>');
+			var altodiv = $('#logs').height();
+			$('#history').scrollTop( altodiv );
 		}
 	});
 

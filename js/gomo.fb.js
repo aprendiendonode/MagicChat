b129067b26ca608b4fc7a4be1a6b2cc4c2bec23a
dannegm <danneg.m@gmail.com>
2012-10-30 16:55:30 -0600

    auto commit

--- js/gomo.fb.js
+++ js/gomo.fb.js
@@ -2,14 +2,13 @@
 Power By Gomosoft
 Modificaciones para éste proyecto por Dannegm
 */
-var log = console.log;
 (function($){ 
 	var boton, on = false, filtro = 0, user, call, access_token, user_id;
 	this.ini =  function(vars){
 		return chkLogin(fbIni());
 	};
 	this.isOn = function(){ 
-		log('Comrpbamos si esta adentro');
+		console.log('Comrpbamos si esta adentro');
 		return on; 
 	}; 
 	this.obtAcToken = function(){ 
@@ -20,29 +19,29 @@ var log = console.log;
 	}
 	this.chkLogin =  function (resp) {
 		if(filtro > 0) return;
-		log('Comprobamos que este logueado');
+		console.log('Comprobamos que este logueado');
 		if (resp.authResponse) {
-			log('Esta loguado, Obtenemos token e id');
+			console.log('Esta loguado, Obtenemos token e id');
 			access_token = resp.authResponse.accessToken;
 			user_id = resp.authResponse.userID;
 			on = true;
 			if(call) call;
 		} else {
-			log('No está logueado');
+			console.log('No está logueado');
 			on = false;
-			log('Le damos poder al boton');
+			console.log('Le damos poder al boton');
 			boton.click(function() {
-				log('Inciamos con facebook');
+				console.log('Inciamos con facebook');
 				FB.login(function(resp) {
-					log('Comprobamos el logueo')
+					console.log('Comprobamos el logueo');
 					if (resp.authResponse) {
 						on = true; 
-						log('Obtenemos token e id')
+						console.log('Obtenemos token e id');
 						access_token = resp.authResponse.accessToken;
 						user_id = resp.authResponse.userID;
 						if(call) call;
 					} else {
-						log('Puto facebook e.e');
+						console.log('Puto facebook e.e');
 						return {cod:11, estado:"sin permisos"};
 					}
 				}, 'email,user_about_me,user_hometown,user_interests,user_location');
@@ -61,7 +60,7 @@ var log = console.log;
 		}, callback );
 	};
 	this.fbIni = function() {
-		log('Configuramos facebook');
+		console.log('Configuramos facebook');
 		FB.init({
 			appId: '125054150878675', 
 			status: true,
@@ -69,19 +68,19 @@ var log = console.log;
 			xfbml: true,
 			oauth: true
 		});
-		log('Revisamos si está loguado');
+		console.log('Revisamos si está logueado');
 		FB.getLoginStatus( chkLogin ); 
-		log('Esto no se para que es');
+		console.log('Esto no se para que es');
 		FB.Event.subscribe( 'auth.statusChange', chkLogin );
 		return this;
 	};
 	jQuery.fn.fb = function(callback){
-		log('Agregamos funcion al boton');
+		console.log('Agregamos funcion al boton');
 		boton = $(this); 
 		if(callback) {
-			log('Seteamos callback');
+			console.log('Seteamos callback');
 			call = callback;
-			log('Iniciamos fb');
+			console.log('Iniciamos fb');
 			return fbIni();
 		}
 	};

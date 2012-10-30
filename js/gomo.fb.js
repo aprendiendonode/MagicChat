cd86fe16943d850551cf166c7acc940d35ef1fd7
dannegm <danneg.m@gmail.com>
2012-10-30 16:51:39 -0600

    auto commit

--- js/gomo.fb.js
+++ js/gomo.fb.js
@@ -9,6 +9,7 @@ var log = console.log;
 		return chkLogin(fbIni());
 	};
 	this.isOn = function(){ 
+		log('Comrpbamos si esta adentro');
 		return on; 
 	}; 
 	this.obtAcToken = function(){ 
@@ -19,26 +20,34 @@ var log = console.log;
 	}
 	this.chkLogin =  function (resp) {
 		if(filtro > 0) return;
+		log('Comprobamos que este logueado');
 		if (resp.authResponse) {
+			log('Esta loguado, Obtenemos token e id');
 			access_token = resp.authResponse.accessToken;
 			user_id = resp.authResponse.userID;
 			on = true;
+			if(call) call;
 		} else {
+			log('No está logueado');
 			on = false;
+			log('Le damos poder al boton');
 			boton.click(function() {
+				log('Inciamos con facebook');
 				FB.login(function(resp) {
+					log('Comprobamos el logueo')
 					if (resp.authResponse) {
 						on = true; 
+						log('Obtenemos token e id')
 						access_token = resp.authResponse.accessToken;
 						user_id = resp.authResponse.userID;
-						location.reload(); 
+						if(call) call;
 					} else {
+						log('Puto facebook e.e');
 						return {cod:11, estado:"sin permisos"};
 					}
 				}, 'email,user_about_me,user_hometown,user_interests,user_location');
 			}); 
 		}
-		if(call) call;
 		filtro++;
 	}; 
 	this.fbInfo = function(callback){
@@ -50,11 +59,9 @@ var log = console.log;
 			method: 'fql.query', 
 			query: query 
 		}, callback );
-	}; 
-	this.actUser = function(user){
-		user = user;
-	}; 
+	};
 	this.fbIni = function() {
+		log('Configuramos facebook');
 		FB.init({
 			appId: '125054150878675', 
 			status: true,
@@ -62,13 +69,20 @@ var log = console.log;
 			xfbml: true,
 			oauth: true
 		});
+		log('Revisamos si está loguado');
 		FB.getLoginStatus( chkLogin ); 
+		log('Esto no se para que es');
 		FB.Event.subscribe( 'auth.statusChange', chkLogin );
 		return this;
 	};
 	jQuery.fn.fb = function(callback){
+		log('Agregamos funcion al boton');
 		boton = $(this); 
-		if(callback) call = callback;
-		return fbIni();
+		if(callback) {
+			log('Seteamos callback');
+			call = callback;
+			log('Iniciamos fb');
+			return fbIni();
+		}
 	};
 })(jQuery);

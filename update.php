e0dad8b4d9a6dbfd3f832fb3bdd366b06ee36ce1
dannegm <danneg.m@gmail.com>
2012-11-04 00:07:13 -0600

    auto commit

--- update.php
+++ update.php
@@ -1,9 +1,3 @@
 <?php
-$out = array();
-$cmd = exec('sh updategit.sh', $out);
-echo "<ol>";
-for($i = 0; $i < count($out); $i++){
-	echo "<li>" . $out[$i] . "</li>";
-}
-echo "</ol>";
+$cmd = exec('sh updategit.sh');
 ?>

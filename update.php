32b20f367b210d1843c19bc3a6f57117496a611c
dannegm <danneg.m@gmail.com>
2012-11-04 00:11:37 -0600

    auto commit

--- update.php
+++ update.php
@@ -1,4 +1,9 @@
 <?php
-exec('sh updategit.sh');
-echo "Auto pull"
+$cmd = exec('sh updategit.sh');
+echo "Auto pull\n"
+if ($cmd){
+	echo "Pull exitoso";
+}else{
+	echo "No se pudo hacer pull";
+}
 ?>
